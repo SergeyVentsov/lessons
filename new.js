d30370295069fc5af6c486fe5befe2671f82c52b
Serg <ventsovsi@mail.ru>
2019-07-26 12:40:26 +0300

    второй урок v.1

--- new.js
+++ new.js
@@ -3,25 +3,29 @@
 let money = prompt("Ваш бюджет на месяц?","100"),
     time = prompt("Введите дату в формате YYYY-MM-DD","2019-07-03");
     
-let question11 = prompt("Введите обязательную статью расходов в этом месяце","Фрукты"),
-    question12 = prompt("Во сколько обойдется?","255"),
-    question21 = prompt("Введите обязательную статью расходов в этом месяце","Овощи"),
-    question22 = prompt("Во сколько обойдется?","355");
-
-let expenses_ = {
-    'question11':  question12,
-    'question21':  question22
-}
-
 let appData = {
     budjet: money,
     timeData: time,
-    expenses: expenses_,
+    expenses: {},
     OptionalExpenses: {},
     income: "",
     savings: false
 }
 
-console.log("Ваш бюджет на 1 день составляет " + appData.budjet/30);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется?");
+    if ((a != '' ) && (b != '') && (typeof(a) != null) && (typeof(b) != null) && (typeof(a) === 'string') && (a.length < 50)) {
+        appData.expenses[a] = b;
+    } else {
+        i--;
+        continue;
+    } 
+
+}
+
+
+appData.moneyPerDay = appData.budjet / 30;
+
 
 console.log(money, time, appData);
