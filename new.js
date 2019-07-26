67a8574e6fa1d2559ffead3ecbad5e79f4286c70
Serg <ventsovsi@mail.ru>
2019-07-26 14:08:15 +0300

    третий урок

--- new.js
+++ new.js
@@ -1,7 +1,18 @@
 'use strict'; // проверка соответствия стандарту ES6
 
-let money = prompt("Ваш бюджет на месяц?","100"),
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?");
     time = prompt("Введите дату в формате YYYY-MM-DD","2019-07-03");
+
+    while (isNaN(money) || money == '' || money == null) { //true если не цифры, пустая строка, оли нажата Отмена
+        money = +prompt("Ваш бюджет на месяц?");
+    }
+}
+
+start();
+
     
 let appData = {
     budjet: money,
@@ -9,23 +20,35 @@ let appData = {
     expenses: {},
     OptionalExpenses: {},
     income: "",
-    savings: false
+    savings: true
 }
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце"),
-        b = prompt("Во сколько обойдется?");
-    if ((a != '' ) && (b != '') && (typeof(a) != null) && (typeof(b) != null) && (typeof(a) === 'string') && (a.length < 50)) {
-        appData.expenses[a] = b;
-    } else {
-        i--;
-        continue;
-    } 
-
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце"),
+            b = prompt("Во сколько обойдется?");
+        if ((a != '' ) && (b != '') && (typeof(a) != null) && (typeof(b) != null) && (typeof(a) === 'string') && (a.length < 50)) {
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        } 
+    }
 }
 
+chooseExpenses();
 
-appData.moneyPerDay = appData.budjet / 30;
+appData.moneyPerDay = (appData.budjet / 30).toFixed() ; //toFixed преобразует в строку
 
 
 console.log(money, time, appData);
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt('Какова сумма накоплений?'),
+            percent = +prompt('Под какой процент?');
+        appData.monthIncome = save/100/12*percent;
+        alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+    }
+}
+
+checkSavings();
